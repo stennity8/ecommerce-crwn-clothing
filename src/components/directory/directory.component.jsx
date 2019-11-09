5e820a51bb1f38d66f06206df7433d848029777b
Shawn <shawntennity@gmail.com>
2019-11-09 17:58:34 -0500

    Created components for menu-items and directory.  Added some initial styling tocomponents.

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import sections from './directory.data';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+export class Directory extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sections: sections
+    }
+  }
+
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title.toUpperCase()} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
